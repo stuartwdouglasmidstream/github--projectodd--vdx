2b8085931344c4a43452023657f06a11821d3265
Toby Crawley <toby@tcrawley.org>
2016-08-05 11:47:33 -0400

    implement UnsupportedElementHandler

--- core/src/main/java/org/projectodd/vdx/core/handlers/UnsupportedElementHandler.java
+++ core/src/main/java/org/projectodd/vdx/core/handlers/UnsupportedElementHandler.java
@@ -1,12 +1,22 @@
 package org.projectodd.vdx.core.handlers;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import org.projectodd.vdx.core.DocElement;
 import org.projectodd.vdx.core.ErrorHandler;
+import org.projectodd.vdx.core.I18N;
 import org.projectodd.vdx.core.ValidationContext;
 import org.projectodd.vdx.core.ValidationError;
 
 public class UnsupportedElementHandler implements ErrorHandler {
     @Override
-    public HandledResult handle(ValidationContext ctx, ValidationError err) {
-        return null;
+    public HandledResult handle(ValidationContext ctx, ValidationError error) {
+        final String el = error.element().getLocalPart();
+        final Set<String> alts = error.alternatives();
+
+        return HandledResult.from(error)
+                .message(I18N.Key.ELEMENT_UNSUPPORTED, el, alts.stream().findFirst().orElse(null));
     }
 }
